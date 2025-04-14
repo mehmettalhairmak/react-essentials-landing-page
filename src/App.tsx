50d617549acbfaee36b66053b3726d1c39fb8a0c
Mehmet Talha Irmak <mehmetirmaakk@gmail.com>
2025-04-14 23:36:19 +0300

    Refactor App component to use React fragments instead of a div for improved rendering efficiency.

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -69,7 +69,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
